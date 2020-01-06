3272b7931d8b702ac272a583179b939759ce6bc7
Sheng Shi <shisheng@sunlands.com>
2020-01-06 12:21:26 +0800

    add file

--- xxl-job-executor/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
+++ xxl-job-executor/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
@@ -3,6 +3,8 @@ package com.xxl.job.executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XxlJobExecutorApplication {
 
 	public static void main(String[] args) {
+		System.out.println("start :\n\r"+System.getProperties());
+		System.out.println("args:\n\r"+ Arrays.toString(args));
         SpringApplication.run(XxlJobExecutorApplication.class, args);
 	}
 
